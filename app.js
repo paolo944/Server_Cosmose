ce66a99a251731443c0d4229aab2c52610e3fea3
Paul Mekhail <paolomekhail4@gmail.com>
2023-03-03 00:25:57 +0100

    correction json2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use('express.json());
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
